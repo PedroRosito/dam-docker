ecd645ed36f2313a762f4fee002931eca0a2184b
Your Name <you@example.com>
2022-10-24 22:33:57 -0300

    clase 2

--- src/frontend/ts/main.ts
+++ src/frontend/ts/main.ts
@@ -1,6 +1,11 @@
 
-function SayHello(){
-    let current_value = document.getElementById("textarea_1") as HTMLInputElement;
-    let new_value = "Hello world!!!" + "\n" + current_value.value;
-    document.getElementById("textarea_1").innerHTML = new_value;
+window.onload = inicio;
+
+function inicio(){
+    let btn = document.getElementById("btnSaludar");
+    btn.onclick = saluar;
+}
+
+function saluar() {
+    alert("Hola");
 }
