36bd887e9f7a0929dbe6c1d3101ea633fc868842
pedro <rositopedro@gmail.com>
2023-04-16 16:10:00 -0300

    Se añade autenticacion utilizando jwt

--- src/backend/index.js
+++ src/backend/index.js
@@ -3,6 +3,7 @@
 var PORT    = 3000;
 
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 
 var express = require('express');
 var app     = express();
@@ -11,6 +12,27 @@ var pool   = require('./mysql-connector');
 var corsOptions = {
     origin: '*',
     optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
+
+// DECLARE JWT-secret
+const JWT_Secret = 'your_secret_key';
+
+var testUser = { username: 'test', password: '1234' };
+
+const auth = function (req, res, next) {
+    let autHeader = (req.headers.authorization || '')
+    if (autHeader.startsWith('Bearer ')) {
+        token = autHeader.split(' ')[1]
+    } else {
+        res.status(401).send({ message: "No hay token en la cabecera" })
+    }
+    jwt.verify(token, JWT_Secret, function(err) {
+        if (err) {
+            console.log('error en el token')
+            res.status(403).send({ meesage: "Token inválido" })
+        }
+    })
+    next()
 }
 
 // to parse application/json
@@ -26,7 +48,7 @@ app.get('/', function(req, res, next) {
     res.send({'mensaje': 'Hola DAM'}).status(200);
 });
 
-app.get('/devices/', function(req, res, next) {
+app.get('/devices/', auth, function(req, res, next) {
     pool.query('Select * from Devices', function(err, result, fields) {
         if (err) {
             res.send(err).status(400);
@@ -35,6 +57,35 @@ app.get('/devices/', function(req, res, next) {
         res.send(result);
     });
 });
+
+app.post('/authenticate', (req, res) => {
+
+    if (req.body) {
+        var user = req.body;
+        console.log(user);
+
+        if (testUser.username === req.body.username && testUser.password === req.body.password) {
+            var token = jwt.sign(user, JWT_Secret);
+            res.status(200).send({
+                signed_user: user,
+                token: token
+            });
+        } else {
+            res.status(403).send({
+                errorMessage: 'Auth required!'
+            });
+        }
+    } else {
+        res.status(403).send({
+            errorMessage: 'Please provide username and password'
+        });
+    }
+
+});
+
+app.get('/prueba', auth, function(req, res) {
+    res.send({ meesage: "Todo ok mando data" })
+})
 
 app.listen(PORT, function(req, res) {
     console.log("NodeJS API running correctly");
