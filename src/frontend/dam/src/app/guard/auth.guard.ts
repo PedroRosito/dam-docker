74616310778896dbab38c1c19ea3b68abaaac24c
pedro <rositopedro@gmail.com>
2023-04-16 16:12:53 -0300

    Se añade guarda de autenticacion

---
+++ src/frontend/dam/src/app/guard/auth.guard.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+import { LoginService } from '../services/login.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard {
+
+  constructor(private _loginService: LoginService, private _router: Router) {}
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    
+    if (!this._loginService.logIn) {
+      this._router.navigate(['/login'])
+      return false
+    }
+
+    return true;
+  }
+}
